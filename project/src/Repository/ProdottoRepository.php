b6919d3a0131bdc30023287527c70f48275cc763
tony <agosta@netseven.it>
2025-03-23 21:49:01 +0100

    code cleaning

--- project/src/Repository/ProdottoRepository.php
+++ project/src/Repository/ProdottoRepository.php
@@ -15,29 +15,4 @@ class ProdottoRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Prodotto::class);
     }
-
-    //    /**
-    //     * @return Prodotto[] Returns an array of Prodotto objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Prodotto
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
